f6626e6d4747f9e1d8bd71d1407ac6aa26b429b1
Sam Ghatak <samanwayghatak@gmail.com>
2022-02-10 10:48:23 +0530

    Fixing incorrect hint.

--- fileserviceinterface.py
+++ fileserviceinterface.py
@@ -1,5 +1,5 @@
 class FileServiceInterface:
     def save_file(self, file_storage_object) -> str:
         pass
-    def search_file(self, filename) -> (bool, str):
+    def search_file(self, filename):
         pass
