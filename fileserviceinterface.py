7877b96257a883c405b305f48baf43814600b184
Sam Ghatak <samanwayghatak@gmail.com>
2022-02-10 11:32:11 +0530

    Fixing procfile and correcting return type hint.

--- fileserviceinterface.py
+++ fileserviceinterface.py
@@ -1,5 +1,8 @@
+from typing import Tuple
+
+
 class FileServiceInterface:
     def save_file(self, file_storage_object) -> str:
         pass
-    def search_file(self, filename):
+    def search_file(self, filename) -> Tuple[bool, str]:
         pass
